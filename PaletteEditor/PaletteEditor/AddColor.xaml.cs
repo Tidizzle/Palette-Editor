1b0dcaa8e9ce1c7bff41c2ca5161ff3e8d5e04d5
Zach Taylor <zach@planetjester.com>
2017-01-11 14:19:47 -0500

    Initial Commit

---
+++ PaletteEditor/PaletteEditor/AddColor.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AnchorMapLib;
+
+namespace PaletteEditor
+{
+    /// <summary>
+    /// Interaction logic for AddColor.xaml
+    /// </summary>
+    public partial class AddColor : Window
+    {
+        private ListView Grid;
+        private MainWindow Main;
+
+        public AddColor(ListView ColorGrid, MainWindow Main)
+        {
+            InitializeComponent();
+            Grid = ColorGrid;
+            this.Main = Main;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ColorTile color = new ColorTile();
+            color.Name = NameBox.Text;
+
+            if (!int.TryParse(IDBox.Text, out color.ID))
+            {
+                IDBox.Clear();
+                MessageBox.Show("Incorrect ID value input", "Add color", MessageBoxButton.OK);
+            }
+            else
+            {
+                if (int.TryParse(RBox.Text, out color.r) && int.TryParse(GBox.Text, out color.g) && int.TryParse(BBox.Text, out color.b) && int.TryParse(ABox.Text, out color.a))
+                {
+                    if (color.r < 0 || color.r > 255 || color.g < 0 || color.g > 255 || color.b < 0 || color.b > 255 || color.a < 0 || color.a > 255)
+                    {
+                        MessageBox.Show("RGBA values must be greater than 0 and less than 255", "Add Color", MessageBoxButton.OK);
+                    }
+                    else
+                    {
+                        Main.Palette.ColorList.Add(color);
+                        var NameList = new List<string>();
+                        foreach (var tile in Main.Palette.ColorList)
+                        {
+                            NameList.Add(tile.Name.ToString());
+                        }
+
+                        Grid.ItemsSource = Main.Palette.ColorList;
+                        Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("incorrect RGB value input");
+                }
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ PaletteEditor/PaletteEditor/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AnchorMapLib;
+using System.IO;
+
+namespace PaletteEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Palette Palette;
+        public bool PaletteOpen;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddColor_Click(object sender, RoutedEventArgs e)
+        {
+            if (PaletteOpen)
+            {
+                AddColor AddColor_win = new AddColor(List, this);
+                AddColor_win.Show();
+            }
+            else
+            {
+                MessageBox.Show("You must be editing a palette to add colors", "Add Color Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void RemoveColor_Click(object sender, RoutedEventArgs e)
+        {
+            if (PaletteOpen)
+            {
+                if (List.SelectedIndex == -1)
+                {
+                    MessageBox.Show("You must select a color", "Remove Color Error", MessageBoxButton.OK);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must be editing a palette to remove colors", "Remove Color Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void OpenPalette_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openDialog = new Microsoft.Win32.OpenFileDialog();
+            openDialog.DefaultExt = ".palette";
+            openDialog.Filter = "Palette File | *.palette";
+            openDialog.Title = "Open Palette File";
+
+            var result = openDialog.ShowDialog();
+
+            if (result == true)
+            {
+                if (!openDialog.FileName.Contains(".palette"))
+                {
+                    MessageBox.Show("Incorrect File Type", "File Open Error", MessageBoxButton.OK);
+                }
+                else
+                {
+                    PaletteOpen = true;
+                    Palette = new Palette();
+                    Palette.ColorList = new List<ColorTile>();
+
+                    //ADD DESERIALIZATION AND SETTING PALETTE VAR TO WHAT IS PASSED BACK EX Palette = Maplib.Deserialize();
+
+                    Palette = new Palette() { paletteName = "TEST PALETTE", colors = 5 };
+
+                    NameBox.Text = Palette.paletteName;
+                    CountBox.Text = Palette.colors.ToString();
+                }
+            }
+        }
+
+        private void SavePalette_Click(object sender, RoutedEventArgs e)
+        {
+            if (!PaletteOpen)
+            {
+                MessageBox.Show("You must be editing a palette to save", "Palette Saving Error", MessageBoxButton.OK);
+            }
+            else
+            {
+                //ADD SERIALIZATION AND FILE WRITING
+            }
+        }
+
+        private void SaveAsPalette_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog SaveDialog = new Microsoft.Win32.SaveFileDialog();
+            SaveDialog.DefaultExt = ".palette";
+            SaveDialog.Title = "Save Palette File";
+
+            if (PaletteOpen)
+            {
+                SaveDialog.FileName = Palette.fileName;
+            }
+            else
+            {
+                SaveDialog.FileName = "NewPalette";
+            }
+            SaveDialog.Filter = "Palette File |*.palette";
+
+            var result = SaveDialog.ShowDialog();
+
+            if (result == true)
+            {
+                Palette.fileName = SaveDialog.FileName;
+                //ADD PALETTE SERIALIZATION
+
+                File.WriteAllText(Palette.fileName, "!!!!!!!!!!!!!!!!!!!!fix this");
+            }
+        }
+
+        private void Creator_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.github.com/tidizzle");
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            var AboutWin = new AboutWin();
+            AboutWin.Show();
+        }
+
+        private void NewPalette_Click(object sender, RoutedEventArgs e)
+        {
+            if (PaletteOpen)
+            {
+                var result = MessageBox.Show("Do you want to save your current Palette?", "File Loss Warning", MessageBoxButton.YesNoCancel);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        SaveAsPalette_Click(this, new RoutedEventArgs());
+                        break;
+
+                    case MessageBoxResult.Cancel:
+                        break;
+                }
+
+                if (result == MessageBoxResult.Yes || result == MessageBoxResult.No)
+                {
+                    NewPalette NewWin = new NewPalette(NameBox, CountBox, this);
+                    NewWin.Show();
+                }
+            }
+            else
+            {
+                NewPalette Win = new NewPalette(NameBox, CountBox, this);
+                Win.Show();
+            }
+
+            PaletteOpen = true;
+        }
+
+        private void List_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+    }
+}
